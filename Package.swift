7a013db1590dae58c8dbcae6497b7beea99cb13a
Emil Karimov <emvakar@gmail.com>
2023-10-03 17:33:22 +0300

    update

--- Package.swift
+++ Package.swift
@@ -1,11 +1,16 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "EKAstrologyCalc",
-    platforms: [.iOS(.v11), .macOS(.v10_12)],
+    platforms: [
+        .iOS(.v11),
+        .macOS(.v12),
+        .tvOS(.v11),
+        .watchOS(.v5),
+    ],
     products: [
         .library(name: "EKAstrologyCalc", targets: ["EKAstrologyCalc"]),
     ],
@@ -13,7 +18,11 @@ let package = Package(
         .package(url: "https://github.com/ESKARIA/ESDateHelper.git", from: "1.1.0")
     ],
     targets: [
-        .target(name: "EKAstrologyCalc", dependencies: ["ESDateHelper"]),
-        .testTarget(name: "EKAstrologyCalcTests", dependencies: ["EKAstrologyCalc"]),
+        .target(name: "EKAstrologyCalc", dependencies: [
+            "ESDateHelper"
+        ]),
+        .testTarget(name: "EKAstrologyCalcTests", dependencies: [
+            "EKAstrologyCalc"
+        ]),
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import EKAstrologyCalcTests
-
-var tests = [XCTestCaseEntry]()
-tests += EKAstrologyCalcTests.allTests()
-XCTMain(tests)
